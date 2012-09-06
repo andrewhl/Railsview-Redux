25c40a7cc41d435afbed526167a74065d24b5015
andrew <ahlavats@rogers.com>
2012-09-06 18:54:27 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsviewRedux::Application.config.secret_token = 'd18daa9c6aa1fca5044e7314cd94cc353d9b5afd218a679381295881dedf2aa2c2d9e090fb0fa6481d0363fe4daa9b092914953fec23260dcf29d8a46ae6594c'
